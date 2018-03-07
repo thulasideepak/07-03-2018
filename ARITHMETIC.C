f40cf1d4392c9b6d29e6904ef99ad0ba2d8859ff
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-03-07 16:16:49 +0530

    Create ARITHMETIC.C
    multiplication division

---
+++ ARITHMETIC.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int a,b,c,n;
+ 	printf("\n enter the value of a :");
+ 	scanf("%d",&a);
+        printf("\n enter the value of b :");
+ 	scanf("%d",&b);
+ 	printf("\n enter the value of c :");
+ 	scanf("%d",&c);
+ 	n=(a*b)/c;
+ 	printf("\n result = %d",n);
+ 	return 0;
+ }
